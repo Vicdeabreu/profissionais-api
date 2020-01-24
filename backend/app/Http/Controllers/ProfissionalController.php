946ce5da3e7ca7ad5782740f60bc63b1c6b0e405
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2020-01-24 12:09:14 -0300

    Cadastro de Profissional com technology

--- backend/app/Http/Controllers/ProfissionalController.php
+++ backend/app/Http/Controllers/ProfissionalController.php
@@ -4,20 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Profissional;
+use App\Technology;
 
 class ProfissionalController extends Controller
 {
     public function listarProfissionais(Request $request){
-        $listaProfissionais = Profissional::all();
+        $listaProfissionais = Profissional::find(6);
 
-        return response()->json($listaProfissionais);
+        return response()->json($listaProfissionais->technologies);
     }
 
     public function criarProfissional(Request $request){
+        $technologyId = $request->technology;
         $newProfissional = new Profissional();
         $newProfissional->name = $request->name;
         $newProfissional->github = $request->github;
         $result = $newProfissional->save();
+        $technology = Technology::find($technologyId);
+
+        if($technology){
+            $technology->profissionais()->attach($newProfissional->id);
+        }else{
+            return response()->json(["error"=>"O id da tecnologia não existe"]);
+        }
 
         return response()->json($newProfissional);
     }
--- backend/app/Profissional.php
+++ backend/app/Profissional.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Profissional extends Model
 {
     protected $table = "profissionais";
+    // Solo para aclarar el nombre de la tabla y no tener conflicto
     public function technologies(){
         return $this->hasMany('App\Technology', 'profissionais_technology', "profissionais_id", "technologies_id");
+    // De esta forma no hace falta incluir el modelo arriba
     }
 }
