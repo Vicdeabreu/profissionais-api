a532269dd7f39404c81cfc2719f10ebc9e6b7e81
Victor De Abreu <vmanuel.deabreu1@gmail.com>
2020-01-27 11:41:32 -0300

    terminando a criação da API profissionais

--- backend/app/Profissional.php
+++ backend/app/Profissional.php
@@ -9,7 +9,11 @@ class Profissional extends Model
     protected $table = "profissionais";
     // Solo para aclarar el nombre de la tabla y no tener conflicto
     public function technologies(){
-        return $this->hasMany('App\Technology', 'profissionais_technology', "profissionais_id", "technologies_id");
+        return $this->belongsToMany(
+            'App\Technology', 
+            'profissionais_technology', 
+            'profissionais_id', 
+            'technologies_id');
     // De esta forma no hace falta incluir el modelo arriba
     }
 }
